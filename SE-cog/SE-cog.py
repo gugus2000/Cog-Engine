82c664613bbe22c070d04d176c93e870d7548155
gugus2000 <gugus.demagny@gmail.com>
2019-01-22 20:02:51 +0100

    little improvements

--- SE-cog/SE-cog.py
+++ SE-cog/SE-cog.py
@@ -22,7 +22,7 @@ class SEcog:
         if context.invoked_subcommand is None:
             await send_cmd_help(context)
 
-    @_SEcog.command(pass_context=True, name='SEnews', aliases=['SEN', 'SEn', 'sen', 'SENews', 'SENEWS', 'senews'])
+    @_SEcog.command(pass_context=True, name='news', aliases=['SEN', 'SEn', 'sen', 'SENews', 'SENEWS', 'senews', 'SEnews'])
     async def _SEnews(self):
         '''
         Get the last news of Space Engine
@@ -31,11 +31,15 @@ class SEcog:
         async with aiohttp.get(urlSEnews) as response:
             soupObject = BeautifulSoup(await response.text(), "html.parser")
         try:
+            dateMonthNews1 = soupObject.find(class_='wrapper').find(class_='content').find(class_='container').find(class_='post').find(class_='post_image').find(class_='post_date_standard_holder').find(class_='post_date_month').get_text()
+            dateDayNews1 = soupObject.find(class_='wrapper').find(class_='content').find(class_='container').find(class_='post').find(class_='post_image').find(class_='post_date_standard_holder').find(class_='post_date_day').get_text()
+            dateYearNews1 = soupObject.find(class_='wrapper').find(class_='content').find(class_='container').find(class_='post').find(class_='post_image').find(class_='post_date_standard_holder').find(class_='post_date_year').get_text()
+            dateNews1 = dateDayNews1 + ' ' + dateMonthNews1 + ' ' + dateYearNews1
             nomNews1 = soupObject.find(class_='wrapper').find(class_='content').find(class_='container').find(class_='post').find(class_='post_text').find('h2').find('a').get_text()
-            soulignementNews1 = "="*len(nomNews1)
+            soulignementNews1 = "="*(len(nomNews1)+len(dateNews1)+5)
             contenuNews1 = soupObject.find(class_='wrapper').find(class_='content').find(class_='container').find(class_='post').find(class_='post_text').find(class_='post_excerpt').get_text()
             lienNews1 = soupObject.find(class_='wrapper').find(class_='content').find(class_='container').find(class_='post').find(class_='post_text').find('h2').find('a')['href']
-            await self.bot.say("```markdown\n" + nomNews1 + "\n" + soulignementNews1 + "\n" + contenuNews1 + "\n[Lien vers la news](" + lienNews1 + ")\n```")
+            await self.bot.say("```markdown\n" + dateNews1 + ' - ' + nomNews1 + "\n" + soulignementNews1 + "\n" + contenuNews1 + "\n[Lien vers la news](" + lienNews1 + ")\n```")
         except:
             await self.bot.say("L'information n'existe pas: la page " + urlSEnews + " a été supprimée ou son architecture modifiée.")
 
